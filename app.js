dace92e1613b552951a935fb06ca6e075bef48a7
Chalermchai Viriyamanatham <Chalermchai_v@kkumail.com>
2021-08-15 17:22:26 +0700

    Initialize commit, Add nodemon yargs chalk

---
+++ app.js
@@ -0,0 +1,63 @@
+const chalk = require('chalk')
+const { describe, command } = require('yargs')
+const yargs = require('yargs')
+const notes = require('./notes')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            description: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body:', argv.body)
+    }
+})
+
+// Crate remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    hadler: function () {
+        console.log('Removing the note!')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function () {
+        console.log('List all the notes!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function() {
+        console.log('Read the note!')
+    }
+})
+
+
+// add, remove, read, list
+
+yargs.parse()
+
+// console.log(yargs.argv)
